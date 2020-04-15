67ba2497297d8b7718878537056a53ba9a8a9b3c
Ivo Minic <ivo.minic@gmail.com>
2020-04-15 11:56:45 +0200

    Napravljen prikaz slike
    Kreiran div za pretragu

--- js/zbunje.js
+++ js/zbunje.js
@@ -1,9 +1,9 @@
-var domainUrl = 'http://167.172.171.249' //location.origin;
+var domainUrl = location.origin; //"http://167.172.171.249"; //location.origin;
 var wmsUrl = domainUrl + "/geoserver/winsoft/wms";
 var imageUrl = domainUrl + "/slike/";
 var tiledRaster = new ol.layer.Tile({
   source: new ol.source.OSM(),
-  name: "Podloga"
+  name: "Podloga",
 });
 /*var overlay = new ol.Overlay({
   position: "bottom-center",
@@ -11,9 +11,31 @@ var tiledRaster = new ol.layer.Tile({
 });*/
 
 var draw, modify;
-var idObjekta, akcija = 'pan',
-  oblik = 'linija';
+var idObjekta,
+  akcija = "pan",
+  oblik = "linija",
+  slikaUrl = "";
 
+/**Stilizacija vektora */
+var fill = new ol.style.Fill({
+  color: "rgba(255,0,0,0.3)",
+});
+var stroke = new ol.style.Stroke({
+  color: "#ff0000",
+  width: 2,
+});
+var circle = new ol.style.Circle({
+  radius: 7,
+  fill: fill,
+  stroke: stroke,
+});
+var vectorStyle = new ol.style.Style({
+  fill: fill,
+  stroke: stroke,
+  image: circle,
+});
+
+/**Povezivanje kontrola sa akcijama */
 document.querySelector("#pan").addEventListener("click", pan);
 document.querySelector("#odaberi").addEventListener("click", odaberi);
 document.querySelector("#dodaj").addEventListener("click", dodaj);
@@ -33,7 +55,6 @@ document.querySelector("#btnPonisti").addEventListener("click", ponisti);
 //document.querySelector("#btnFilter").addEventListener("click", filtriranje);
 document.querySelector("#btnIzbrisi").addEventListener("click", brisanje);
 
-
 function popuniKontrole(odgovor) {
   var atributi = odgovor.features[0]["properties"];
   idObjekta = odgovor.features[0]["id"];
@@ -46,16 +67,17 @@ function popuniKontrole(odgovor) {
   document.querySelector("#zdravstvenoStanje").value = atributi["zdravstveno_stanje"];
   document.querySelector("#napomena").value = atributi["napomena"];
 
-
   //slika
   var slika = atributi["url_fotografije"];
   slika.length && (slika = slika.substring(slika.lastIndexOf("/") + 1, slika.length));
-  //overlay.getElement().innerHTML = '<a target="_blank" href="' + imageUrl + slika + '"><img src="' + imageUrl + slika + '"></a>';
+  slika.length && (slikaUrl = imageUrl + slika);
 
+  //document.querySelector("#slika").innerHTML = '<a target="_blank" href="' + imageUrl + slika + '"><i class="fas fa-camera-retro"></i></a>';
+  //overlay.getElement().innerHTML = '<a target="_blank" href="' + imageUrl + slika + '"><img src="' + imageUrl + slika + '"></a>';
 }
 
 function sacuvaj() {
-  return false
+  return false;
   if (!idObjekta) {
     alert("Potrebno je odabrati objekat za koji se unose podaci.");
     return false;
@@ -68,7 +90,7 @@ function sacuvaj() {
       narodniNaziv: document.querySelector("#narodniNaziv").value,
       tip: document.querySelector("#tip").value,
       zdravstvenoStanje: document.querySelector("#zdravstvenoStanje").value,
-      napomena: document.querySelector("#napomena").value
+      napomena: document.querySelector("#napomena").value,
     },
     type: "GET",
     timeout: 10000,
@@ -79,7 +101,7 @@ function sacuvaj() {
     },
     success: function (response) {
       restartovanje();
-    }
+    },
   });
 }
 
@@ -91,6 +113,7 @@ function restartovanje() {
   document.querySelector("#tip").value = "";
   document.querySelector("#zdravstvenoStanje").value = "";
   document.querySelector("#napomena").value = "";
+  slikaUrl = "";
 
   //overlay.getElement().innerHTML = "";
 }
@@ -101,171 +124,118 @@ var rasterLayer = new ol.layer.Image({
   source: new ol.source.ImageWMS({
     url: wmsUrl,
     params: {
-      LAYERS: "winsoft:zbunje"
+      LAYERS: "winsoft:zbunje",
     },
     ratio: 1,
-    serverType: "geoserver"
-  })
+    serverType: "geoserver",
+  }),
 });
 
 var center = ol.proj.transform([19.26, 42.443], "EPSG:4326", "EPSG:3857");
 var view = new ol.View({
   center: center,
-  zoom: 17
+  zoom: 17,
 });
 
 var map = new ol.Map({
   target: "map",
   layers: [tiledRaster, rasterLayer], //
-  view: view
+  view: view,
 });
 
 var featuresLine = new ol.Collection();
 var featureLineOverlay = new ol.layer.Vector({
   source: new ol.source.Vector({
-    features: featuresLine
+    features: featuresLine,
   }),
-  style: new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(255, 255, 255, 0.2)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#ff0000',
-      width: 2
-    }),
-    image: new ol.style.Circle({
-      radius: 7,
-      fill: new ol.style.Fill({
-        color: '#ff0000'
-      })
-    })
-  })
+  style: vectorStyle,
 });
 featureLineOverlay.setMap(map);
 
 var featuresPoint = new ol.Collection();
 var featurePointOverlay = new ol.layer.Vector({
   source: new ol.source.Vector({
-    features: featuresPoint
+    features: featuresPoint,
   }),
-  style: new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(255, 255, 255, 0.2)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#ff0000',
-      width: 2
-    }),
-    image: new ol.style.Circle({
-      radius: 7,
-      fill: new ol.style.Fill({
-        color: '#ff0000'
-      })
-    })
-  })
+  style: vectorStyle,
 });
 featurePointOverlay.setMap(map);
 
 var featuresPolygon = new ol.Collection();
 var featurePolygonOverlay = new ol.layer.Vector({
   source: new ol.source.Vector({
-    features: featuresPolygon
+    features: featuresPolygon,
   }),
-  style: new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(255, 0, 0, 0.4)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#ff0000',
-      width: 2
-    }),
-    image: new ol.style.Circle({
-      radius: 7,
-      fill: new ol.style.Fill({
-        color: '#ff0000'
-      })
-    })
-  })
+  style: vectorStyle,
 });
 featurePolygonOverlay.setMap(map);
 
-
-/*var modify = new ol.interaction.Modify({
-  features: featuresLine,
-  // the SHIFT key must be pressed to delete vertices, so
-  // that new vertices can be drawn at the same position
-  // of existing vertices
-  deleteCondition: function (event) {
-    return ol.events.condition.shiftKeyOnly(event) &&
-      ol.events.condition.singleClick(event);
-  }
+/**Za crtanje i izmjenu geometrije */
+var featuresTekuci = new ol.Collection();
+var featureTekuciOverlay = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    features: featuresTekuci,
+  }),
+  style: vectorStyle,
 });
-map.addInteraction(modify);*/
-
+featureTekuciOverlay.setMap(map);
 
 function podesiInterakciju() {
   //uklanja draw i modify
   map.removeInteraction(draw);
   map.removeInteraction(modify);
 
-  if (akcija === 'marker') {
+  if (akcija === "marker") {
     draw = new ol.interaction.Draw({
       features: featuresPoint,
-      type: 'Point'
+      type: "Point",
     });
     map.addInteraction(draw);
   }
-  if (akcija === 'linija') {
+  if (akcija === "linija") {
     draw = new ol.interaction.Draw({
       features: featuresLine,
-      type: 'LineString'
+      type: "LineString",
     });
     map.addInteraction(draw);
   }
-  if (akcija === 'poligon') {
+  if (akcija === "poligon") {
     draw = new ol.interaction.Draw({
       features: featuresPolygon,
-      type: 'Polygon'
+      type: "Polygon",
     });
     map.addInteraction(draw);
   }
-  if (akcija === 'izmijeni') {
-    if (oblik === 'Point') {
+  if (akcija === "izmijeni") {
+    if (oblik === "Point") {
       modify = new ol.interaction.Modify({
         features: featuresPoint,
         deleteCondition: function (event) {
-          return ol.events.condition.shiftKeyOnly(event) &&
-            ol.events.condition.singleClick(event);
-        }
+          return ol.events.condition.shiftKeyOnly(event) && ol.events.condition.singleClick(event);
+        },
       });
     }
-    if (oblik === 'LineString') {
+    if (oblik === "LineString") {
       modify = new ol.interaction.Modify({
         features: featuresLine,
         deleteCondition: function (event) {
-          return ol.events.condition.shiftKeyOnly(event) &&
-            ol.events.condition.singleClick(event);
-        }
+          return ol.events.condition.shiftKeyOnly(event) && ol.events.condition.singleClick(event);
+        },
       });
     }
-    if (oblik === 'Polygon') {
+    if (oblik === "Polygon") {
       modify = new ol.interaction.Modify({
         features: featuresPolygon,
         deleteCondition: function (event) {
-          return ol.events.condition.shiftKeyOnly(event) &&
-            ol.events.condition.singleClick(event);
-        }
+          return ol.events.condition.shiftKeyOnly(event) && ol.events.condition.singleClick(event);
+        },
       });
     }
     map.addInteraction(modify);
   }
-
 }
 
 //addInteraction();
-
-
-
 
 //map.on("pointermove", onMouseMove);
 
@@ -283,7 +253,7 @@ function onMouseMove(evt) {
   map.getTargetElement().style.cursor = "";
   var pixel = map.getEventPixel(evt.originalEvent);
   var hit = map.forEachLayerAtPixel(pixel, function (layer) {
-    if (layer.B.name === 'zbunje') {
+    if (layer.B.name === "zbunje") {
       console.log(layer.B);
       console.log("ima");
       map.getTargetElement().style.cursor = "pointer";
@@ -294,18 +264,18 @@ function onMouseMove(evt) {
 }
 
 var dragAndDrop = new ol.interaction.DragAndDrop({
-  formatConstructors: [ol.format.GPX, ol.format.GeoJSON, ol.format.IGC, ol.format.KML, ol.format.TopoJSON]
+  formatConstructors: [ol.format.GPX, ol.format.GeoJSON, ol.format.IGC, ol.format.KML, ol.format.TopoJSON],
 });
 
 dragAndDrop.on("addfeatures", function (event) {
   var vectorSource = new ol.source.Vector({
     features: event.features,
-    projection: event.projection
+    projection: event.projection,
   });
   map.getLayers().push(
     new ol.layer.Vector({
       source: vectorSource,
-      style: vectorStyle
+      style: vectorStyle,
     })
   );
   view.fitExtent(vectorSource.getExtent(), map.getSize());
@@ -316,12 +286,12 @@ map.addInteraction(dragAndDrop);
 map.on("click", onMouseClick);
 
 function onMouseClick(browserEvent) {
-  if (akcija === 'atributi' || akcija === 'odaberi') {
+  if (akcija === "atributi" || akcija === "odaberi") {
     var coordinate = browserEvent.coordinate;
     var pixel = map.getPixelFromCoordinate(coordinate);
 
     var url = rasterLayer.getSource().getGetFeatureInfoUrl(browserEvent.coordinate, map.getView().getResolution(), "EPSG:3857", {
-      INFO_FORMAT: "application/json"
+      INFO_FORMAT: "application/json",
     });
     if (url) {
       fetch(url)
@@ -343,31 +313,30 @@ function onMouseClick(browserEvent) {
   //map.addOverlay(overlay);
 }
 
-
 function pan() {
   setujAktivnu("#pan");
-  akcija = 'pan';
+  akcija = "pan";
 }
 
 function odaberi() {
   setujAktivnu("#odaberi");
-  akcija = 'odaberi';
+  akcija = "odaberi";
 }
 
 function dodaj() {
   setujAktivnu("#dodaj");
-  akcija = 'dodaj';
-  oblik = 'LineString';
+  akcija = "dodaj";
+  oblik = "LineString";
 }
 
 function izbrisi() {
   setujAktivnu("#izbrisi");
-  akcija = 'izbrisi';
+  akcija = "izbrisi";
 }
 
 function izmijeni() {
   setujAktivnu("#izmijeni");
-  akcija = 'izmijeni';
+  akcija = "izmijeni";
   //oblik = 'LineString';
   podesiInterakciju();
 }
@@ -375,38 +344,45 @@ function izmijeni() {
 function atributi() {
   setujAktivnu("#atributi");
   showDiv("#atributiDiv");
-  akcija = 'atributi';
+  closeDiv("#pretragaDiv");
+  akcija = "atributi";
 }
 
 function marker() {
   setujAktivnu("#marker");
-  akcija = 'marker';
-  oblik = 'Point';
+  akcija = "marker";
+  oblik = "Point";
   podesiInterakciju();
 }
 
 function pretraga() {
   setujAktivnu("#pretraga");
   closeDiv("#atributiDiv");
-  akcija = 'pretraga';
+  showDiv("#pretragaDiv");
+  akcija = "pretraga";
 }
 
 function slika() {
-  setujAktivnu("#slika");
-  akcija = 'slika';
+  if (slikaUrl === "") {
+    poruka("Upozorenje", "Nije odabran objekat na mapi za koji želite da se prikaže fotografija.");
+  } else {
+    setujAktivnu("#slika");
+    akcija = "slika";
+    window.open(slikaUrl, "_blank");
+  }
 }
 
 function linija() {
   setujAktivnu("#linija");
-  akcija = 'linija';
-  oblik = 'LineString';
+  akcija = "linija";
+  oblik = "LineString";
   podesiInterakciju();
 }
 
 function poligon() {
   setujAktivnu("#poligon");
-  akcija = 'poligon';
-  oblik = 'Polygon';
+  akcija = "poligon";
+  oblik = "Polygon";
   podesiInterakciju();
 }
 
@@ -427,12 +403,14 @@ function brisanje() {
   console.log("brisanje");
 }
 
-
 function setujAktivnu(element) {
-
-  var els = document.querySelectorAll('.active');
+  var els = document.querySelectorAll(".active");
   for (var i = 0; i < els.length; i++) {
-    els[i].classList.remove('active')
+    els[i].classList.remove("active");
   }
   document.querySelector(element).classList.add("active");
+}
+
+function poruka(naslov, tekst) {
+  alert(naslov, tekst);
 }
